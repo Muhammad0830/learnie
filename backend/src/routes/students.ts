dda4f98b84b22fbe34a6bdacad2e5ea8c0d4c5cb
Muhammad0830 <abduqayumovm1@gmail.com>
2025-11-01 15:32:57 +0500

    backend is connected to the database, added some functions for the future changes

--- backend/src/routes/students.ts
+++ backend/src/routes/students.ts
@@ -1,15 +1,40 @@
 import express from "express";
-import { query } from "../utils/helper";
+import { queryGlobal, queryUniversity } from "../utils/helper";
 import { RowDataPacket, ResultSetHeader } from "mysql2";
 
 const studentsRouter = express.Router();
 
+const students = [
+  {
+    id: 1,
+    title: "Student 1",
+    content: "This is the first Student.",
+  },
+  {
+    id: 2,
+    title: "Student 2",
+    content: "This is the second Student.",
+  },
+];
+
 studentsRouter.get("/", async (request, response) => {
-  const rows = await query<RowDataPacket[]>(
-    "SELECT * FROM students WHERE id = ?",
-    request.query.id
-  );
-  response.json(rows);
+  response.json(students);
+});
+
+studentsRouter.get("/all", async (request, response) => {
+  try {
+    const rows = await queryGlobal<RowDataPacket[]>("SELECT * FROM students");
+
+    if (rows.length === 0) {
+      response.status(404).json({ error: "No students found" });
+      return;
+    }
+
+    console.log("students", rows);
+    response.status(200).json(rows);
+  } catch (error) {
+    response.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 export default studentsRouter;
