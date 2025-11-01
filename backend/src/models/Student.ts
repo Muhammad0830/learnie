7508124a73730c94c3a9608dcb3fae8c887750c3
Muhammad0830 <abduqayumovm1@gmail.com>
2025-11-01 20:21:21 +0500

    request of enrolling a student to the course is created

--- backend/src/models/Student.ts
+++ backend/src/models/Student.ts
@@ -1,7 +1,7 @@
 import { queryGlobal, queryUniversity } from "../utils/helper";
 import { RowDataPacket, ResultSetHeader } from "mysql2";
 
-export async function getStudents({ schemaName }: { schemaName: string }) {
+export async function getStudentsList({ schemaName }: { schemaName: string }) {
   try {
     const rows = await queryUniversity<RowDataPacket[]>(
       schemaName,
@@ -21,6 +21,7 @@ export async function createStudent({
   email,
   phoneNumber,
   studentId,
+  courseIds,
 }: {
   schemaName: string;
   name: string;
@@ -28,6 +29,7 @@ export async function createStudent({
   email: string;
   phoneNumber: string;
   studentId: string;
+  courseIds?: string[];
 }) {
   try {
     const rows = await queryUniversity<ResultSetHeader>(
@@ -36,6 +38,19 @@ export async function createStudent({
         VALUES (:name, :age, :email, :phoneNumber, :studentId)`,
       { name, age: age ?? null, email, phoneNumber, studentId }
     );
+
+    const insertId = rows.insertId;
+
+    if (courseIds) {
+      for (const courseId of courseIds) {
+        await queryUniversity<ResultSetHeader>(
+          schemaName,
+          `INSERT INTO student_courses (student_id, course_id) 
+          VALUES (:student_id, :course_id)`,
+          { course_id: courseId, student_id: insertId }
+        );
+      }
+    }
 
     return {
       id: rows.insertId,
@@ -50,8 +65,8 @@ export async function createStudent({
   }
 }
 
-export async function getStudent({
-  id: studentId,
+export async function getEachStudent({
+  id,
   schemaName,
 }: {
   id: string;
@@ -60,8 +75,11 @@ export async function getStudent({
   try {
     const rows = await queryUniversity<RowDataPacket[]>(
       schemaName,
-      `SELECT * FROM students WHERE studentId = :studentId`,
-      { studentId }
+      `SELECT s.*, sc.course_id, c.name as course_name FROM students as s 
+        LEFT JOIN student_courses as sc ON s.id = sc.student_id
+        LEFT JOIN courses as c ON sc.course_id = c.id
+        WHERE s.id = :id`,
+      { id }
     );
 
     return rows;
