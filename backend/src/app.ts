285f3648651183d79a97dce5ca0cdb27184c03e4
Muhammad0830 <abduqayumovm1@gmail.com>
2025-11-01 14:03:48 +0500

    frontend and backend connection achieved

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,18 @@
 import studentsRouter from "./routes/students";
 import unknownEndpoint from "./middlewares/middleware";
+import cors from "cors";
 
 import express from "express";
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 const notes = [
   {
