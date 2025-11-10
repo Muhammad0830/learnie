8e0169a40f3454b9a929c28f90e2132cb84041dc
Muhammad0830 <abduqayumovm1@gmail.com>
2025-11-10 12:30:24 +0500

    students create page is done

--- frontend_website/src/app/[locale]/(routes)/students/create/page.tsx
+++ frontend_website/src/app/[locale]/(routes)/students/create/page.tsx
@@ -1,7 +1,140 @@
-import React from "react";
+"use client";
+import AddingCourseToStudent from "@/components/students/AddingCourseToStudent";
+import FormCheckDialog from "@/components/students/FormCheckDialog";
+import StudentCreateForm from "@/components/students/StudentCreateForm";
+import { useCustomToast } from "@/context/CustomToastContext";
+import { useApiMutation } from "@/hooks/useApiMutation";
+import useApiQuery from "@/hooks/useApiQuery";
+import { StudentFormData, studentSchema } from "@/schemas/studentSchema";
+import { Course } from "@/types/types";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useTranslations } from "next-intl";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { useForm, useWatch } from "react-hook-form";
 
 const Page = () => {
-  return <div>Create Students Page</div>;
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [isPhoneValid, setIsPhoneValid] = useState(true);
+  const t = useTranslations("Students");
+  const toastT = useTranslations("Toast");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+    control,
+    setValue,
+    trigger,
+  } = useForm<StudentFormData>({
+    resolver: zodResolver(studentSchema),
+    defaultValues: {
+      role: "student",
+    },
+  });
+  const { showToast } = useCustomToast();
+  const router = useRouter();
+
+  const { mutate } = useApiMutation<{ success: boolean }, StudentFormData>(
+    "/users",
+    "post"
+  );
+
+  const {
+    data: courses,
+    isLoading,
+    isError,
+    error,
+  } = useApiQuery<Course[]>("/courses", { key: "CourseList" });
+
+  const onSubmit = (data: StudentFormData) => {
+    console.log("Form submitted:", data);
+
+    mutate(data, {
+      onSuccess: () => {
+        reset();
+        setIsDialogOpen(false);
+        showToast("success", toastT("Student created successfully"));
+        router.push("/students");
+      },
+      onError: (error) => {
+        console.error("student create failed", error);
+      },
+    });
+  };
+
+  if (isError) {
+    showToast("error", toastT("Error Fetching Courses"));
+    console.error("Error fetching courses:", error);
+  }
+
+  const selectedCoursesIds = useWatch({
+    control,
+    name: "coursesIds",
+  });
+
+  const selectedCourses = courses?.filter((course) =>
+    selectedCoursesIds?.includes(String(course.id))
+  );
+
+  console.log("selectedCoursesIds", selectedCoursesIds);
+
+  return (
+    <div>
+      <div className="flex items-center justify-between gap-4 mb-4">
+        <h1 className="lg:text-3xl md:text-2xl text-xl font-bold">
+          {t("Create New Student")}
+        </h1>
+
+        <Link
+          href={"/students"}
+          className="rounded-sm px-3 py-1.5 cursor-pointer bg-primary/30 hover:bg-primary/60 dark:bg-primary/50 dark:hover:bg-primary/30 border border-primary text-black dark:text-white sm:text-[16px] text-xs"
+        >
+          {t("Back")}
+        </Link>
+      </div>
+
+      <StudentCreateForm
+        onSubmit={handleSubmit(onSubmit)}
+        errors={errors}
+        register={register}
+        control={control}
+        setValue={setValue}
+        onPhoneValidityChange={setIsPhoneValid}
+      />
+
+      <AddingCourseToStudent
+        isLoading={isLoading}
+        courses={courses ?? []}
+        selectedCoursesIds={selectedCoursesIds}
+        setValue={setValue}
+      />
+
+      <button
+        type="button"
+        onClick={async () => {
+          const isValid = await trigger();
+          if (!isPhoneValid) {
+            showToast("error", "Invalid phone number");
+            return;
+          }
+          setIsDialogOpen(isValid);
+        }}
+        className="mt-4 cursor-pointer px-3 py-1.5 rounded-sm bg-primary/20 hover:bg-primary/30 transition-colors duration-150 border border-primary"
+      >
+        {t("Submit")}
+      </button>
+
+      <FormCheckDialog
+        isDialogOpen={isDialogOpen}
+        setIsDialogOpen={setIsDialogOpen}
+        control={control}
+        selectedCourses={selectedCourses ?? []}
+        isSubmitting={isSubmitting}
+      />
+    </div>
+  );
 };
 
 export default Page;
---
+++ frontend_website/src/components/students/StudentCreateForm.tsx
@@ -0,0 +1,180 @@
+"use client";
+import { cn } from "@/lib/utils";
+import { StudentFormData } from "@/schemas/studentSchema";
+import { useTranslations } from "next-intl";
+import React, { useState } from "react";
+import {
+  Control,
+  FieldErrors,
+  UseFormRegister,
+  UseFormSetValue,
+} from "react-hook-form";
+import { PhoneInput } from "./PhoneInput";
+
+type StudentCreateFormProps = {
+  onSubmit: React.FormEventHandler<HTMLFormElement>;
+  errors: FieldErrors<StudentFormData>;
+  register: UseFormRegister<StudentFormData>;
+  control: Control<StudentFormData>;
+  setValue: UseFormSetValue<StudentFormData>;
+  onPhoneValidityChange?: (isValid: boolean) => void; // ✅ new
+};
+
+const StudentCreateForm = ({
+  onSubmit,
+  errors,
+  register,
+  control,
+  setValue,
+  onPhoneValidityChange,
+}: StudentCreateFormProps) => {
+  const t = useTranslations("Students");
+
+  return (
+    <form id="student_create_form" onSubmit={onSubmit} className="space-y-4">
+      <div className="flex gap-2">
+        {/* name */}
+        <div className="relative flex-1">
+          <label className="block mb-0.5 font-semibold">{t("Name")}</label>
+          <input
+            type="text"
+            {...register("name")}
+            className={cn(
+              "border border-foreground/60 rounded p-2 w-full h-10 bg-background-secondary",
+              errors.name && "border-red-600"
+            )}
+            placeholder={t("EnterStudentName") as string}
+          />
+          {errors.name && (
+            <p className="text-red-500 sm:text-sm absolute text-xs">
+              {t(`${errors.name.message}`)}
+            </p>
+          )}
+        </div>
+
+        {/* email */}
+        <div className="relative flex-1">
+          <label className="block mb-0.5 font-semibold">{t("Email")}</label>
+          <input
+            type="email"
+            {...register("email")}
+            className={cn(
+              "border border-foreground/60 rounded p-2 w-full h-10 bg-background-secondary",
+              errors.name && "border-red-600"
+            )}
+            placeholder={t("EnterStudentEmail") as string}
+          />
+          {errors.email && (
+            <p className="text-red-500 sm:text-sm absolute text-xs">
+              {t(`${errors.email.message}`)}
+            </p>
+          )}
+        </div>
+      </div>
+
+      <div className="flex gap-2">
+        {/* phone number */}
+        <div className="relative flex-1">
+          <label className="block mb-0.5 font-semibold">
+            {t("PhoneNumber")}
+          </label>
+          <PhoneInput
+            className="rounded"
+            value={control._formValues.phoneNumber}
+            onChange={(value) => {
+              if (value) {
+                setValue("phoneNumber", value);
+              }
+            }}
+            onValidityChange={onPhoneValidityChange}
+            placeholder={t("EnterStudentPhoneNumber") as string}
+          />
+          {errors.phoneNumber && (
+            <p className="text-red-500 sm:text-sm absolute text-xs">
+              {t(`${errors.phoneNumber.message}`)}
+            </p>
+          )}
+        </div>
+
+        {/* studentId */}
+        <div className="relative flex-1">
+          <label className="block mb-0.5 font-semibold">{t("studentId")}</label>
+          <input
+            type="text"
+            {...register("studentId", {
+              required: t("studentId is required") as string,
+              pattern: {
+                value: /^[0-9]+$/,
+                message: t("StudentIdMustBeNumber") as string,
+              },
+              onChange: (e) => {
+                e.target.value = e.target.value.replace(/[^0-9]/g, "");
+              },
+            })}
+            className={cn(
+              "border border-foreground/60 rounded p-2 w-full h-10 bg-background-secondary",
+              errors.studentId && "border-red-600"
+            )}
+            placeholder={t("EnterStudentStudentId") as string}
+          />
+          {errors.studentId && (
+            <p className="text-red-500 sm:text-sm absolute text-xs">
+              {t(`${errors.studentId.message}`)}
+            </p>
+          )}
+        </div>
+      </div>
+
+      <div className="flex gap-2">
+        {/* age */}
+        <div className="relative flex-1">
+          <label className="block mb-0.5 font-semibold">{t("Age")}</label>
+          <input
+            type="text"
+            {...register("age", {
+              required: t("AgeIsRequired") as string,
+              pattern: {
+                value: /^[0-9]+$/,
+                message: t("AgeMustBeNumber") as string,
+              },
+              onChange: (e) => {
+                e.target.value = e.target.value.replace(/[^0-9]/g, "");
+              },
+            })}
+            className={cn(
+              "border border-foreground/60 rounded p-2 w-full h-10 bg-background-secondary",
+              errors.age && "border-red-600"
+            )}
+            placeholder={t("EnterStudentAge") as string}
+          />
+          {errors.age && (
+            <p className="text-red-500 sm:text-sm absolute text-xs">
+              {t(`${errors.age.message}`)}
+            </p>
+          )}
+        </div>
+
+        {/* password */}
+        <div className="relative flex-1">
+          <label className="block mb-0.5 font-semibold">{t("password")}</label>
+          <input
+            type="text"
+            {...register("password")}
+            className={cn(
+              "border border-foreground/60 rounded p-2 w-full h-10 bg-background-secondary",
+              errors.password && "border-red-600"
+            )}
+            placeholder={t("EnterStudentPassword") as string}
+          />
+          {errors.password && (
+            <p className="text-red-500 sm:text-sm absolute text-xs">
+              {t(`${errors.password.message}`)}
+            </p>
+          )}
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default StudentCreateForm;
