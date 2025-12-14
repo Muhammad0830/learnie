7a9c5252426cff5a62ac778ef8019f344af18274
Muhammad0830 <abduqayumovm1@gmail.com>
2025-12-14 11:33:25 +0500

    sidebar profile button is enhanced

--- frontend_website/src/components/header_sidebar/ProfileButton.tsx
+++ frontend_website/src/components/header_sidebar/ProfileButton.tsx
@@ -1,22 +1,26 @@
 "use client";
+import { useAuth } from "@/context/AuthContext";
 import { usePathname } from "@/i18n/navigation";
 import { cn } from "@/lib/utils";
 import { AnimatePresence, motion } from "framer-motion";
 import { User } from "lucide-react";
-import { useTranslations } from "next-intl";
 import Link from "next/link";
-import React from "react";
 
 const ProfileButton = ({ collapsed }: { collapsed: boolean }) => {
-  const t = useTranslations("NavBar");
   const pathName = usePathname();
+  const { user } = useAuth();
+
+  if (!user) {
+    return null;
+  }
+
   return (
     <div className="w-full flex justify-center items-center relative p-2">
       <Link
         href={"/profile"}
         className={cn(
-          "text-black dark:text-white px-3 py-2 w-full rounded-sm cursor-pointer hover:bg-primary hover:text-white flex items-center transition-colors duration-100",
-          pathName.includes("/profile") && "bg-primary text-white"
+          "text-black dark:text-white border border-primary hover:border-white px-2 py-1.5 w-full group rounded-sm cursor-pointer hover:bg-primary hover:text-white flex items-center transition-colors duration-100",
+          pathName.includes("/profile") && "bg-primary text-white border-white"
         )}
       >
         <AnimatePresence>
@@ -24,9 +28,15 @@ const ProfileButton = ({ collapsed }: { collapsed: boolean }) => {
             initial={{ marginRight: 8 }}
             animate={collapsed ? { marginRight: 0 } : { marginRight: 8 }}
             layout
-            className="ml-0.5 py-0.5"
           >
-            <User className="w-5 h-5" />
+            <div
+              className={cn(
+                "p-1 rounded-full border border-primary group-hover:border-white transition-colors duration-100",
+                pathName.includes("/profile") && "border-white"
+              )}
+            >
+              <User className="w-5 h-5" />
+            </div>
           </motion.div>
         </AnimatePresence>
         <AnimatePresence mode="wait" initial={false}>
@@ -36,9 +46,14 @@ const ProfileButton = ({ collapsed }: { collapsed: boolean }) => {
               animate={{ opacity: 1, x: 0 }}
               exit={{ opacity: 0, x: 40 }}
               transition={{ duration: 0.2 }}
-              className="whitespace-nowrap text-base/4"
+              className="whitespace-nowrap text-base/4 flex flex-col gap-0.5 w-full"
             >
-              {t("Profile")}
+              <span className="text-base/4 font-bold w-full truncate">
+                {user.name}
+              </span>
+              <span className="text-xs/3 text-foreground/70 w-full group-hover:text-white/70 truncate transition-colors duration-100">
+                {user.email}
+              </span>
             </motion.div>
           )}
         </AnimatePresence>
